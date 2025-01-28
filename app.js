d91055620290583c52e6e2d3e5823b66b91f1464
CCZ <cesarcantelli@gmail.com>
2025-01-28 10:20:43 -0300

    Se modificó el archivo app.js, se mejoró el código de ingreso de nombres. Ahora ya no acepta números ni vacíos en el ingreso de nombres

--- app.js
+++ app.js
@@ -4,32 +4,29 @@ let listaAmigos = [];
 
 function agregarAmigo(){
     let nombreIngresado = document.getElementById('amigo').value;
+    let soloLetras = /^[A-Za-záéíóúñ ]+$/.test(nombreIngresado);
     //nombreIngresado == '' ? alert('Por favor, inserte un nombre') : listaAmigos.push(nombreIngresado);
-    if (nombreIngresado == ''){
-        alert('Por favor, inserte un nombre');
-    } else {
+    if (soloLetras == true && nombreIngresado != ' '){ //Evitamos números y espacio incial
         listaAmigos.push(nombreIngresado);
-        let linea = document.createElement("li");
+        let linea = document.createElement("li"); // Vamos formando la lista
         let texto = document.createTextNode(nombreIngresado);
         linea.appendChild(texto);
         document.getElementById("listaAmigos").appendChild(linea);
+    } else {
+        alert('Por favor, inserte un nombre correctamente.');
     }
     limpiarCaja();
-    console.log(nombreIngresado);
-    console.log(listaAmigos);
 }
 
 function sortearAmigo(){
-    if(listaAmigos.length == []){
+    if(listaAmigos.length == []){ // Si es una lista vacía y pulsamos botón Sortear amigo
         return(document.getElementById("listaAmigos").innerHTML = "No se ha ingresado nombres");
     }
-    let indiceGenerado = Math.floor(Math.random()*listaAmigos.length);
+    let indiceGenerado = Math.floor(Math.random()*listaAmigos.length); // Indice sorteado, entre cero y length -1
     sorteado = "<li><font color='green'>El amigo secreto sorteado es: " + listaAmigos[indiceGenerado] + "</font></li>";
-    console.log(listaAmigos.length);
-    console.log(indiceGenerado);
     document.getElementById("listaAmigos").innerHTML = sorteado;
 }
 
-function limpiarCaja() {
+function limpiarCaja() { // Luego de ingresar nombre borramos box
     document.querySelector('#amigo').value = '';
 }
