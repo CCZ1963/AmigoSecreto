617963bdbe1c95487f2bd45a5a171a9ae9d80ee6
CCZ1963 <cesarcantelli@gmail.com>
2025-02-05 22:22:21 -0300

    Se modificó la aplicación, se le agregó un botón para reiniciar el programa. Además, se modificaron los archivos app.js, style.css y el Readme.md

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 let listaAmigos = [];
 
 function agregarAmigo(){
-    let nombreIngresado = document.getElementById('amigo').value;
-    let soloLetras = /^[A-Za-záéíóúñ ]+$/.test(nombreIngresado);
-    //nombreIngresado == '' ? alert('Por favor, inserte un nombre') : listaAmigos.push(nombreIngresado);
-    if (soloLetras == true && nombreIngresado != ' '){ //Evitamos números y espacio incial
-        listaAmigos.push(nombreIngresado);
-        let linea = document.createElement("li"); // Vamos formando la lista
+    let nombreIngresado = document.getElementById('amigo').value; // Ingresando el nombre
+    let soloLetras = /^[A-Za-záéíóúñ]+$/.test(nombreIngresado); // Controlamos el buen ingreso de no
+    if (soloLetras == true){
+        listaAmigos.push(nombreIngresado); // Se ingresa nombre al array
+        // Si no hay dos nombres ingresados no se activa botón "Sortear Amigo"
+        if(listaAmigos.length > 1){document.getElementById('sorteo').removeAttribute('disabled')};
+        let linea = document.createElement("li"); // Vamos formando la lista, se imprime en pantalla
         let texto = document.createTextNode(nombreIngresado);
         linea.appendChild(texto);
-        document.getElementById("listaAmigos").appendChild(linea);
+        document.getElementById("lineaAmigos").appendChild(linea);
     } else {
-        alert('Por favor, inserte un nombre correctamente.');
+        alert('Por favor, ingrese un nombre correctamente.');
     }
     limpiarCaja();
 }
 
 function sortearAmigo(){
-    if(listaAmigos.length == []){ // Si es una lista vacía y pulsamos botón Sortear amigo
-        return(document.getElementById("listaAmigos").innerHTML = "No se ha ingresado nombres");
-    }
-    let indiceGenerado = Math.floor(Math.random()*listaAmigos.length); // Indice sorteado, entre cero y length -1
+    let indiceGenerado = Math.floor(Math.random()*listaAmigos.length); // Indice sorteado, entre cero y length - 1
     sorteado = "<li><font color='green'>El amigo secreto sorteado es: " + listaAmigos[indiceGenerado] + "</font></li>";
-    document.getElementById("listaAmigos").innerHTML = sorteado;
+    document.getElementById("lineaAmigos").innerHTML = sorteado; // Escribimos en pantalla nombre sorteado
+    document.querySelector('#sorteo').setAttribute('disabled','true');  // DEshabilitamos el botón Sortear Amigo
+    document.getElementById('reiniciar').removeAttribute('disabled'); // Habilitamos botón Reiniciar Sorteo
+}
+
+function reiniciarSorteo() {
+    limpiarCaja();
+    listaAmigos = []; //
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    document.getElementById("lineaAmigos").innerHTML = ""; // Limpiamos la pantalla
 }
 
 function limpiarCaja() { // Luego de ingresar nombre borramos box
